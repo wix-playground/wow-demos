60b12b97e1b8fb4150fefbeb8abe70f63e1fa4ac
Yehonatan Daniv <yehonatand@wix.com>
2021-02-23 00:51:24 +0200

    Added dragging of gradients

--- gradients/index.js
+++ gradients/index.js
@@ -6,17 +6,24 @@ const MIN_RADIUS = 10;
 const MAX_RADIUS = 2000;
 const RADIUS_STEP = 1;
 const GENERAL_FIELDS = {
+    ACTION: 'Action',
     SHOW_CIRCLES: 'Show circles',
     BG_COLOR: 'BG color',
 }
 
 const config = {
+    [GENERAL_FIELDS.ACTION]: 'add',
     [GENERAL_FIELDS.SHOW_CIRCLES]: 'last',
     [GENERAL_FIELDS.BG_COLOR]: '#fff'
 };
 
 gui.remember(config);
 
+gui.add(config, GENERAL_FIELDS.ACTION, ['add', 'move'])
+    .onChange(value => {
+        mainEl.dataset.action = value;
+        setAction(value);
+    })
 gui.addColor(config, GENERAL_FIELDS.BG_COLOR)
     .onChange(value => {
         mainEl.style.backgroundColor = value;
@@ -55,7 +62,19 @@ function addFolder ({onColor, onSize, onRemove, onMiddle}) {
 const mainEl = document.querySelector('main');
 const circles = [];
 
-mainEl.addEventListener('click', addCircle);
+function setAction (type) {
+    if (type === 'add') {
+        mainEl.removeEventListener('pointerdown', moveStart);
+        mainEl.removeEventListener('pointerup', moveEnd);
+        mainEl.addEventListener('click', addCircle);
+    }
+    else if (type === 'move') {
+        mainEl.removeEventListener('click', addCircle);
+        mainEl.addEventListener('pointerdown', moveStart);
+        mainEl.addEventListener('pointerup', moveEnd);
+        mainEl.addEventListener('pointermove', checkCircleToMove);
+    }
+}
 
 function addCircle (e) {
     const circle = new Circle({
@@ -67,6 +86,63 @@ function addCircle (e) {
 
     generateGradients();
 }
+
+let hoveredCircle = null;
+let current = {x: 0, y: 0};
+let shouldCheckCircleToMove = true;
+
+function checkCirclesDist () {
+    shouldCheckCircleToMove = true;
+
+    const closest = circles.map(
+        circle => ({
+            dist: Math.hypot(current.x - circle.x, current.y - circle.y),
+            circle
+        })
+    ).sort((a, b) => {
+        return a.dist > b.dist ? 1 : -1;
+    })[0];
+
+    if (hoveredCircle !== closest.circle) {
+        if (hoveredCircle) {
+            delete hoveredCircle.el.dataset.hover;
+        }
+        closest.circle.el.dataset.hover = 'true';
+        hoveredCircle = closest.circle;
+    }
+}
+
+function checkCircleToMove (e) {
+    current.x = e.x;
+    current.y = e.y;
+    if (shouldCheckCircleToMove) {
+        shouldCheckCircleToMove = false;
+        requestAnimationFrame(checkCirclesDist);
+    }
+}
+
+function cancelCheckCircleToMove () {
+    cancelAnimationFrame(checkCirclesDist);
+    shouldCheckCircleToMove = true;
+}
+
+function moveStart (e) {
+    cancelCheckCircleToMove();
+    mainEl.removeEventListener('pointermove', checkCircleToMove);
+    mainEl.addEventListener('pointermove', moveMove);
+}
+
+function moveMove (e) {
+    hoveredCircle.setPosition(e);
+    hoveredCircle.updateGradient();
+}
+
+function moveEnd (e) {
+    mainEl.removeEventListener('pointermove', moveMove);
+    mainEl.addEventListener('pointermove', checkCircleToMove);
+}
+
+setAction(config[GENERAL_FIELDS.ACTION]);
 
 class Circle {
     constructor ({x, y}) {
@@ -98,7 +174,11 @@ class Circle {
         this.el.style.setProperty('--radius', `${this.config.size}px`);
     }
 
-    setPosition () {
+    setPosition (position) {
+        if (position) {
+            this.x = position.x;
+            this.y = position.y;
+        }
         this.el.style.setProperty('--x', `${this.x - this.config.size / 2}px`);
         this.el.style.setProperty('--y', `${this.y - this.config.size / 2}px`);
     }
@@ -108,24 +188,23 @@ class Circle {
         this.gradient = [`${this.config.size}px`, `${this.x}px ${this.y}px`, `rgb(${this.config.color})`, `${this.config.middle}%`];
     }
 
-    onColor () {
+    updateGradient () {
         this.createGradient();
-
         generateGradients();
+    }
+
+    onColor () {
+        this.updateGradient();
     }
 
     onSize () {
         this.setSize();
         this.setPosition();
-        this.createGradient();
-
-        generateGradients();
+        this.updateGradient();
     }
 
     onMiddle () {
-        this.createGradient();
-
-        generateGradients();
+        this.updateGradient();
     }
 
     onRemove () {
