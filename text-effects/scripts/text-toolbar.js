4c09bb94f8fdfbb87685e3690463d2266f884a22
Tom Bigelajzen <tombigel@wix.com>
2022-05-22 16:05:31 +0300

    added support for variant

--- text-effects/scripts/text-toolbar.js
+++ text-effects/scripts/text-toolbar.js
@@ -3,23 +3,26 @@ export function setTextToolbarFontList(fonts) {
     const item = $id('font-family-template').content.firstElementChild;
     const list = $id('font-family-list');
 
-    fonts.forEach(({ family, selected }) => {
-        // Create font item
-        const clone = item.cloneNode(true);
-        const input = clone.querySelector('.input');
-        const content = clone.querySelector('.content');
+    fonts.forEach(({ family, variants, selected }) => {
+        variants.forEach((variant) => {
+            // Create font item
+            const clone = item.cloneNode(true);
+            const familyInput = clone.querySelector('.family');
+            const variantInput = clone.querySelector('.variant');
+            const content = clone.querySelector('.content');
 
-        input.value = family;
-        content.textContent = family;
-        content.style.fontFamily = family;
+            familyInput.value = family;
+            variantInput.value = variant;
+            content.textContent = `${family} ${['400', 'regular'].includes(variant) ? '' : variant}`;
+            content.style.fontFamily = family;
+            content.style.fontWeight = variant;
+            // Set default
+            if (selected) {
+                familyInput.setAttribute('checked', 'checked');
+            }
 
-        // Set default
-        if (selected) {
-            input.setAttribute('checked', 'checked');
-        }
-
-        // Add to document
-        list.appendChild(clone);
+            // Add to document
+            list.appendChild(clone);
+        });
     });
 }
-
