222fb0f28a49478e38fe7b6fda24715a550ced93
Gev <gevk@wix.com>
2023-01-18 17:10:02 +0200

    added ghost

--- section-scroll/index.js
+++ section-scroll/index.js
@@ -16,32 +16,36 @@ const EFFECTS_CONFIG = {
         LABEL: 'Translate X',
         MIN: -window.innerWidth,
         MAX: window.innerWidth,
-        STEP: 5
+        STEP: 5,
     },
     TRANSLATE_Y: {
         LABEL: 'Translate Y',
         MIN: -window.innerHeight,
         MAX: window.innerHeight,
-        STEP: 5
+        STEP: 5,
     },
     ROTATE: {
         LABEL: 'Rotate',
         MIN: -360,
         MAX: 360,
-        STEP: 5
+        STEP: 5,
     },
     SCALE: {
         LABEL: 'Scale',
         MIN: 0,
         MAX: 2,
-        STEP: .1
+        STEP: .1,
     },
     OPACITY: {
         LABEL: 'Opacity',
         MIN: 0,
         MAX: 1,
-        STEP: .1
+        STEP: .1,
     },
+    GHOST: {
+        LABEL: 'Ghost',
+        VALUE: true,
+    }
 };
 
 
@@ -53,6 +57,7 @@ const config = {
             [EFFECTS_CONFIG.ROTATE.LABEL]: 0,
             [EFFECTS_CONFIG.SCALE.LABEL]: 1,
             [EFFECTS_CONFIG.OPACITY.LABEL]: 1,
+            [EFFECTS_CONFIG.GHOST.LABEL]: true,
         }
     }
 };
@@ -102,42 +107,39 @@ function createSection1Folder() {
 
     photo1.add(config[SECTION_1][PHOTO], ...Object.values(EFFECTS_CONFIG.TRANSLATE_X))
         .onChange(val => {
-            const photo = sections[0].querySelector('.photo');
-            const toFrame = sections[0].querySelector('.to-frame');
-            photo.style.setProperty('--x-trans', `${val}px`);
-            toFrame.style.setProperty('--x-trans', `${val}px`);
+            const elements = [...sections[0].querySelectorAll('img')];
+            elements.forEach(e => e.style.setProperty('--x-trans', `${val}px`));
             init();
         })
     photo1.add(config[SECTION_1][PHOTO], ...Object.values(EFFECTS_CONFIG.TRANSLATE_Y))
         .onChange(val => {
-            const photo = sections[0].querySelector('.photo');
-            const toFrame = sections[0].querySelector('.to-frame');
-            photo.style.setProperty('--y-trans', `${val}px`);
-            toFrame.style.setProperty('--y-trans', `${val}px`);
+            const elements = [...sections[0].querySelectorAll('img')];
+            elements.forEach(e => e.style.setProperty('--y-trans', `${val}px`));
             init();
         })
     photo1.add(config[SECTION_1][PHOTO], ...Object.values(EFFECTS_CONFIG.ROTATE))
         .onChange(val => {
-            const photo = sections[0].querySelector('.photo');
-            const toFrame = sections[0].querySelector('.to-frame');
-            photo.style.setProperty('--rotate', `${val}deg`);
-            toFrame.style.setProperty('--rotate', `${val}deg`);
+            const elements = [...sections[0].querySelectorAll('img')];
+            elements.forEach(e => e.style.setProperty('--rotate', `${val}deg`));
             init();
         })
     photo1.add(config[SECTION_1][PHOTO], ...Object.values(EFFECTS_CONFIG.SCALE))
         .onChange(val => {
-            const photo = sections[0].querySelector('.photo');
-            const toFrame = sections[0].querySelector('.to-frame');
-            photo.style.setProperty('--scale', val - 1);
-            toFrame.style.setProperty('--scale', val - 1);
+            const elements = [...sections[0].querySelectorAll('img')];
+            elements.forEach(e => e.style.setProperty('--scale', val - 1));
             init();
         })
     photo1.add(config[SECTION_1][PHOTO], ...Object.values(EFFECTS_CONFIG.OPACITY))
         .onChange(val => {
-            const photo = sections[0].querySelector('.photo');
-            const toFrame = sections[0].querySelector('.to-frame');
-            photo.style.setProperty('--opacity', val - 1);
-            toFrame.style.setProperty('--opacity', val - 1);
+            const elements = [...sections[0].querySelectorAll('img')];
+            elements.forEach(e => e.style.setProperty('--opacity', val - 1));
             init();
         })
+    photo1.add(config[SECTION_1][PHOTO], ...Object.values(EFFECTS_CONFIG.GHOST))
+        .onChange(showGhost => {
+            const elements = [...sections[0].querySelectorAll('img')];
+            elements.forEach(e => e.style.setProperty('--no-ghost', showGhost ? .1 : 0));
+            init();
+        })
+    
 }
